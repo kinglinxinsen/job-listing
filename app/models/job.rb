1f64cf3377563356bb608ce4c66126f954b908e7
zengjinlin <kinglinxinsen@gmail.com>
2017-04-12 06:45:13 +0800

    step 6 order by wage

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,7 +3,7 @@ class Job < ApplicationRecord
   validates :wage_upper_bound, presence: true
   validates :wage_lower_bound, presence: true
   validates :wage_lower_bound, numericality: { greater_than: 0}
-
+  scope :published, -> { where(is_hidden: false) }
   def publish!
     self.is_hidden = false
     self.save
@@ -12,6 +12,15 @@ class Job < ApplicationRecord
     self.is_hidden = true
     self.save
   end
-
+def index
+  @jobs = case params[:order]
+  when 'by_lower_bound'
+    Job.published.order('wage_lower_bound DESC')
+  when 'by_upper_bound'
+    Job.published.order('wage_upper_bound DESC')
+  else
+    Job.published.recent
+  end
+end
 
 end
